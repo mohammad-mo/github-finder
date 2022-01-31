43fa21023bc6518362fea468c06f4647b0456305
Mohammad <mohammadmahone@gmail.com>
2022-01-31 12:13:54 +0330

    Create components and add routing.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import NotFound from './Pages/NotFound'
+import Navbar from './Components/Layout/Navbar'
+import Footer from './Components/Layout/Footer'
+
 const App = () => 
 {
   return (
-    <div className="bg-purple-500">
-      <h1 className='text-xl text-center'>Hello World</h1>
-      <button className='btn'>Click</button>
-    </div>
+    <Router>
+     <div className="flex flex-col justify-between h-screen">
+       <Navbar />
+       <main className='container mx-auto px-3 pb-12'>
+         <Routes>
+           <Route path='/' element={<Home />} />
+           <Route path='/about' element={<About />} />
+           <Route path='/notfound' element={<NotFound />} />
+           <Route path='/*' element={<NotFound />} />
+         </Routes>
+       </main>
+       <Footer />
+     </div> 
+    </Router>
   )
 }
 
---
+++ src/Components/Layout/Navbar.jsx
@@ -0,0 +1,48 @@
+import { FaGithub } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+const Navbar = ({ title }) => 
+{
+    return (
+        <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
+            <div className="container mx-auto">
+                <div className="flex-none px-2 mx-2">
+                    <FaGithub className='inline pr-2 text-3xl' />
+                    <Link 
+                        to={{ pathname: '/' }}
+                        className='text-lg font-bold align-middle'    
+                    >
+                        {title}
+                    </Link>
+                </div>
+                <div className="flex-1 px-2 mx-2">
+                    <div className="flex justify-end">
+                        <Link 
+                            to={{ pathname: '/' }}
+                            className='btn btn-ghost btn-sm rounded-btn mr-1'
+                        >
+                            Home
+                        </Link>
+                        <Link 
+                            to={{ pathname: '/about' }}
+                            className='btn btn-ghost btn-sm rounded-btn'
+                        >
+                            About
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+Navbar.defaultProps = {
+    title: 'Github Finder'
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string
+}
+ 
+export default Navbar

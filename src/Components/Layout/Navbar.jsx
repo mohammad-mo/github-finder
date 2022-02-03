1bc5a11eb989d90a3c2e2eb41a254292ca69605e
Mohammad <mohammadmahone@gmail.com>
2022-02-03 11:13:48 +0330

    Make a responsive navbar.

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -1,39 +1,52 @@
+import { useState } from 'react'
 import { FaGithub } from 'react-icons/fa'
+import { BiMenuAltRight } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 const Navbar = ({ title }) => 
 {
+    const [showMenu, setShowMenu] = useState(false)
+
     return (
-        <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
-            <div className="container mx-auto">
-                <div className="flex-none px-2 mx-2">
-                    <FaGithub className='inline pr-2 text-3xl' />
-                    <Link 
-                        to={{ pathname: '/' }}
-                        className='text-lg font-bold align-middle'    
-                    >
-                        {title}
-                    </Link>
-                </div>
-                <div className="flex-1 px-2 mx-2">
-                    <div className="flex justify-end">
-                        <Link 
-                            to={{ pathname: '/' }}
-                            className='btn btn-ghost btn-sm rounded-btn mr-1'
-                        >
-                            Home
-                        </Link>
-                        <Link 
-                            to={{ pathname: '/about' }}
-                            className='btn btn-ghost btn-sm rounded-btn'
-                        >
-                            About
-                        </Link>
-                    </div>
-                </div>
+      <nav className='mb-12 shadow-lg bg-neutral text-neutral-content sm:py-0'>
+        <div className="flex container px-2 py-5 mx-auto flex-wrap sm:px-1">
+          <div className="flex-none px-1 sm:ml-2">
+            <FaGithub className="inline pr-2 text-3xl" />
+            <Link
+              to={{ pathname: "/" }}
+              className="text-lg font-bold align-middle"
+            >
+              {title}
+            </Link>
+          </div>
+          <div className="flex-1 sm:hidden">
+            <div className="flex justify-end" onClick={() => setShowMenu(!showMenu)}>
+                <BiMenuAltRight className='cursor-pointer' size='2em' />
             </div>
-        </nav>
+          </div>
+          <div className={`w-full sm:ml-2 sm:flex sm:flex-1 sm:justify-end ${showMenu ? '': 'hidden'}`}>
+            <ul className="flex flex-col sm:flex-row items-center mt-2 sm:mt-0">
+                <li>
+                    <Link
+                      to={{ pathname: "/" }}
+                      className="btn btn-ghost btn-sm rounded-btn sm:mr-1 mb-1 sm:mb-0"
+                    >
+                      Home
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                      to={{ pathname: "/about" }}
+                      className="btn btn-ghost btn-sm rounded-btn"
+                    >
+                      About
+                    </Link>
+                </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     )
 }
 
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,8 +2,8 @@ const About = () =>
 {
     return (
         <>
-        <h1 className='text-6xl mb-4'>Github Finder</h1>
-        <p className='mb-4 text-2xl font-light'>
+        <h1 className='text-3xl sm:text-6xl mb-4 '>Github Finder</h1>
+        <p className='mb-4 text-xl sm:text-2xl font-light'>
           A React app to search GitHub profiles and see profile details. This
           project is part of the
           <a href='https://www.udemy.com/course/modern-react-front-to-back/'>

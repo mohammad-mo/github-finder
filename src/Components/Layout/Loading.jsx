ae067121fefa8048cbb38116b6097bb4b4f752eb
Mohammad <mohammadmahone@gmail.com>
2022-02-03 11:25:01 +0330

    Add more margin-top to loading.

--- src/Components/Layout/Loading.jsx
+++ src/Components/Layout/Loading.jsx
@@ -2,7 +2,7 @@ const Loading = () =>
 {
     return (
       <>
-        <div className="w-100 mt-20 text-center mx-auto">
+        <div className="w-full mt-40 text-center mx-auto">
           <div className="outer"></div>
           <div className="middle"></div>
           <div className="inner"></div>
